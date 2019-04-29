b14ec212a41474b238acb8131d3c6557cef48042
GretheSlushice <43374884+GretheSlushice@users.noreply.github.com>
2019-04-29 12:32:27 +0200

    kontrolSkema
    done and done

--- Models/KontrolSkema.cs
+++ Models/KontrolSkema.cs
@@ -1,7 +1,20 @@
-﻿namespace Models
+﻿using System;
+
+namespace Models
 {
     public class KontrolSkema
     {
-        
+        public int ID { get; set; }
+        public int FK_Kolonne { get; set; }
+        public DateTime klokkeslæt { get; set; }
+        public double Ludkoncetration { get; set; }
+        public DateTime VægtKontrol { get; set; }
+        public bool Fustage { get; set; }
+        public int Kvittering { get; set; }
+        public double mS { get; set; }
+        public bool LudKontrol { get; set; }
+        public string Signatur { get; set; }
+        public bool mSKontrol { get; set; }
+        public double Vægt { get; set; }
     }
 }
