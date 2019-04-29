203eeaf7d7c651c4fcd5cd711747ca77700171ea
ThomasSoerensen1996 <thomas10001@live.dk>
2019-04-29 12:34:30 +0200

    Produktionsfølgeseddel
    added Produktionsfølgeseddel class

---
+++ Models/Produktionsfølgeseddel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Produktionsfølgeseddel
+    {
+        public int ID { get; set; }
+        public int FK_Kolonne { get; set; }
+        public DateTime Slut { get; set; }
+        public DateTime Start { get; set; }
+        public int Bemanding { get; set; }
+        public float Timer { get; set; }
+        public string Signatur{ get; set; }
+        public int Pause { get; set; }
+    }
+}
