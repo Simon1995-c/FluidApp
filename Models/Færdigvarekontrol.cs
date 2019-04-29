b9d0f32e9e4f0a2f7d35727a423bd3abcd2d7160
Resul134 <resulkurrr134@gmail.com>
2019-04-29 12:26:02 +0200

    Færdigvarekontrol properties

--- Models/Færdigvarekontrol.cs
+++ Models/Færdigvarekontrol.cs
@@ -7,5 +7,17 @@ namespace Models
 {
     public class Færdigvarekontrol
     {
+        public int ID { get; set; }
+        public int FK_Kolonne { get; set; }
+        public DateTime Klokkeslæt { get; set; }
+        public DateTime Holdbarhedsdato { get; set; }
+        public DateTime Produktionsdato { get; set; }
+        public int FærdigvareNr { get; set; }
+        public string Kommentar { get; set; }
+        public bool Spritkontrol { get; set; }
+        public int HætteNr { get; set; }
+        public int EtiketNr { get; set; }
+        public string Fustage { get; set; }
+        public string Signatur { get; set; }
     }
 }
