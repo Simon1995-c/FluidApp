de6c30ed5b89ae161d2715aa79dacd0c64877c1a
Resul134 <resulkurrr134@gmail.com>
2019-04-29 12:38:24 +0200

    Kontrolregistrering

--- Models/KontrolSkema.cs
+++ Models/KontrolSkema.cs
@@ -1,4 +1,12 @@
-﻿namespace Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Models
 {
     public class KontrolSkema
     {
---
+++ Models/Kontrolregistrering.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Kontrolregistrering
+    {
+        public int ID { get; set; }
+        public int FK_Kolonne { get; set; }
+        public DateTime Klokkeslæt { get; set; }
+        public DateTime Holdbarhedsdato { get; set; }
+        public DateTime Produktionsdato { get; set; }
+        public int FærdigvareNr { get; set; }
+        public string Kommentar { get; set; }
+        public bool Spritkontrol { get; set; }
+        public int HætteNr { get; set; }
+        public int EtiketNr { get; set; }
+        public string Fustage { get; set; }
+        public string Signatur { get; set; }
+    }
+}
