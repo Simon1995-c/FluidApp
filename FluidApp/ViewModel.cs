3842c2ccd2df09379ddf438c4b0773d9e8576be6
alex5955 <43375012+alex5955@users.noreply.github.com>
2019-04-29 12:22:31 +0200

    Fullscreen automatically, Ip range, "forside"

---
+++ FluidApp/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using FluidApp.Annotations;
+
+namespace FluidApp
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> temp { get; set; }
+        public ViewModel()
+        {
+            ipHandler h = new ipHandler();
+
+            //If the IP isn't allowed -> send them to an error page
+            if (!h.isAllowedIp())
+            {
+                var frame = new Frame();
+                frame.Navigate(typeof(errorPageIPrange), null);
+                Window.Current.Content = frame;
+            }
+
+            temp = new ObservableCollection<string>
+            {
+                "Test",
+                "Test",
+                "Test",
+                "Test",
+                "Test",
+                "Test",
+                "Test",
+                "Test",
+                "Test"
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FluidApp/ipHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluidApp
+{
+    class ipHandler
+    {
+        private List<string> allowedIps { get; set; }
+
+        public ipHandler()
+        {
+            allowedIps = new List<string>();
+            allowedIps.Add("185.20.241.84");
+        }
+
+        private static string getIp()
+        {
+            var myIp = new HttpClient().GetStringAsync("https://api.ipify.org/");
+
+            return myIp.Result;
+        }
+
+        public bool isAllowedIp()
+        {
+            string currentIp = getIp();
+            int validIp = 0;
+
+            foreach (var ip in allowedIps)
+            {
+                if (ip == currentIp)
+                {
+                    validIp++;
+                }
+            }
+
+            if (validIp > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
