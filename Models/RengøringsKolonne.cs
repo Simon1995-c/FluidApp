1430952f7e180b5890a800a3cd986ee1ba329056
ThomasSoerensen1996 <thomas10001@live.dk>
2019-04-29 12:30:07 +0200

    RengøringsKolonne
    added RengøringsKolonne class

---
+++ Models/RengøringsKolonne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class RengøringsKolonne
+    {
+        public int ID { get; set; }
+        public int KolonneNr { get; set; }
+        public int UgeNr { get; set; }
+        public string Kommentar { get; set; }
+        public string Opgave { get; set; }
+        public string Udstyr { get; set; }
+        public string VejledningsNr { get; set; }
+        public int Frekvens { get; set; }
+        public DateTime SidstDato { get; set; }
+        public DateTime IgenDato { get; set; }
+        public string Udførsel { get; set; }
+        public string Signatur { get; set; }
+    }
+}
