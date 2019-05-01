6d8fba41883dd841dd1b553ee947a0ab68be4cd8
Resul134 <resulkurrr134@gmail.com>
2019-05-01 13:38:31 +0200

    Rengøring og Produktion

--- Models/Produktionsfølgeseddel.cs
+++ Models/Produktionsfølgeseddel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Models
 {
@@ -16,5 +18,86 @@ namespace Models
         public float Timer { get; set; }
         public string Signatur { get; set; }
         public int Pause { get; set; }
+
+        public const string URI = "https://restapi20190501124159.azurewebsites.net/api/Produktionsfølgeseddel";
+
+
+        public List<Produktionsfølgeseddel> GetAll()
+        {
+            List<Produktionsfølgeseddel> returnList = new List<Produktionsfølgeseddel>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI);
+                string jsonStr = resTask.Result;
+
+                returnList = JsonConvert.DeserializeObject<List<Produktionsfølgeseddel>>(jsonStr);
+            }
+
+            return returnList;
+        }
+
+        public Produktionsfølgeseddel GetOne(int id)
+        {
+            Produktionsfølgeseddel returnOne = new Produktionsfølgeseddel();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI + id);
+                string jsonStr = resTask.Result;
+
+                returnOne = JsonConvert.DeserializeObject<Produktionsfølgeseddel>(jsonStr);
+            }
+
+            return returnOne;
+        }
+
+        public void Delete(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Task<HttpResponseMessage> deleteAsync = client.DeleteAsync(URI + "/" + id);
+
+                HttpResponseMessage resp = deleteAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    String jsonStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+        public void Post(Produktionsfølgeseddel obj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonStr = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(jsonStr, Encoding.ASCII, "application/json");
+
+                Task<HttpResponseMessage> postAsync = client.PostAsync(URI, content);
+
+                HttpResponseMessage resp = postAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+        public void Put(int id, Produktionsfølgeseddel obj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonStr = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
+
+                Task<HttpResponseMessage> putAsync = client.PutAsync(URI + "/" + obj, content);
+
+                HttpResponseMessage resp = putAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
     }
 }
