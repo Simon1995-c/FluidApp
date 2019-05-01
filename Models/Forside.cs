84a5acae0402b534fc841fa5c9ed9f8f6b0a1894
ThomasSoerensen1996 <thomas10001@live.dk>
2019-05-01 13:41:36 +0200

    Forside
    still missing Put

--- Models/Forside.cs
+++ Models/Forside.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Models
 {
@@ -11,9 +13,89 @@ namespace Models
         public int ID { get; set; }
         public int FK_Kolonne { get; set; }
         public int FærdigvareNr { get; set; }
-        public string Færdigvarenavn { get; set; }
-        public int Processordrenummer { get; set; }
+        public string FærdigvareNavn { get; set; }
+        public int ProcessordreNr { get; set; }
         public string Produktionsinitialer { get; set; }
-        public DateTime dato { get; set; }
+        public DateTime Dato { get; set; }
+
+        public const string URI = "https://restapi20190501124159.azurewebsites.net/api/Forsides";
+
+        public List<Forside> GetAll()
+        {
+            List<Forside> returnList = new List<Forside>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI);
+                string jsonStr = resTask.Result;
+
+                returnList = JsonConvert.DeserializeObject<List<Forside>>(jsonStr);
+            }
+
+            return returnList;
+        }
+
+        public Forside GetOne(int id)
+        {
+            Forside returnOne = new Forside();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI + id);
+                string jsonStr = resTask.Result;
+
+                returnOne = JsonConvert.DeserializeObject<Forside>(jsonStr);
+            }
+
+            return returnOne;
+        }
+
+        public void Delete(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Task<HttpResponseMessage> deleteAsync = client.DeleteAsync(URI + "/" + id);
+
+                HttpResponseMessage resp = deleteAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    String jsonStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+        public void Post(Forside obj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonStr = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
+
+                Task<HttpResponseMessage> postAsync = client.PostAsync(URI, content);
+
+                HttpResponseMessage resp = postAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+        public void Put(int id, Forside obj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonStr = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
+
+                Task<HttpResponseMessage> putAsync = client.PutAsync(URI + "/" + id, content);
+
+                HttpResponseMessage resp = putAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
     }
 }
