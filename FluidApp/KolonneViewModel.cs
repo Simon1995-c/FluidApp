1482f56f78be3cab214a5a6e8e42a40f30228b9b
GretheSlushice <43374884+GretheSlushice@users.noreply.github.com>
2019-05-03 14:57:17 +0200

    Kolonne basal frontend

--- FluidApp/KolonneViewModel.cs
+++ FluidApp/KolonneViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using GalaSoft.MvvmLight.Command;
+using Models;
 
 namespace FluidApp
 {
     class KolonneViewModel
     {
+        public RelayCommand TilbageCommand { get; set; }
+        public ObservableCollection<Forside> KolonneListe { get; set; }
+
         public KolonneViewModel()
         {
+            TilbageCommand = new RelayCommand(Tilbage);
             ipHandler h = new ipHandler();
 
             //If the IP isn't allowed -> send them to an error page
@@ -21,6 +28,29 @@ namespace FluidApp
                 frame.Navigate(typeof(errorPageIPrange), null);
                 Window.Current.Content = frame;
             }
+
+            KolonneListe = GetDatasets();
+        }
+
+        public void Tilbage()
+        {
+            var frame = new Frame();
+            frame.Navigate(typeof(MainPage), null);
+            Window.Current.Content = frame;
+        }
+
+        public ObservableCollection<Forside> GetDatasets()
+        {
+            Forside tempForside = new Forside();
+            ObservableCollection<Forside> data = new ObservableCollection<Forside>();
+
+            foreach (var att in tempForside.GetAll())
+            {
+                
+                
+                data.Add(att);
+            }
+            return data;
         }
     }
 }
