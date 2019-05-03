073588bc574945d11f220abb310966fc9d682e69
ThomasSoerensen1996 <thomas10001@live.dk>
2019-05-03 10:45:19 +0200

    Kontrolregistrering
    Alle 5 metoder virker

--- Models/Kontrolregistrering.cs
+++ Models/Kontrolregistrering.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Models
 {
@@ -20,5 +22,85 @@ namespace Models
         public int EtiketNr { get; set; }
         public string Fustage { get; set; }
         public string Signatur { get; set; }
+
+        public const string URI = "https://restapi20190501124159.azurewebsites.net/api/Kontrolregistrerings";
+
+        public List<Kontrolregistrering> GetAll()
+        {
+            List<Kontrolregistrering> returnList = new List<Kontrolregistrering>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI);
+                string jsonStr = resTask.Result;
+
+                returnList = JsonConvert.DeserializeObject<List<Kontrolregistrering>>(jsonStr);
+            }
+
+            return returnList;
+        }
+
+        public Kontrolregistrering GetOne(int id)
+        {
+            Kontrolregistrering returnOne = new Kontrolregistrering();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> resTask = client.GetStringAsync(URI + id);
+                string jsonStr = resTask.Result;
+
+                returnOne = JsonConvert.DeserializeObject<Kontrolregistrering>(jsonStr);
+            }
+
+            return returnOne;
+        }
+
+        public void Delete(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Task<HttpResponseMessage> deleteAsync = client.DeleteAsync(URI + "/" + id);
+
+                HttpResponseMessage resp = deleteAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    String jsonStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+        public void Post(Kontrolregistrering obj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonStr = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
+
+                Task<HttpResponseMessage> postAsync = client.PostAsync(URI, content);
+
+                HttpResponseMessage resp = postAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+        public void Put(int id, Kontrolregistrering obj)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonStr = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
+
+                Task<HttpResponseMessage> putAsync = client.PutAsync(URI + "/" + id, content);
+
+                HttpResponseMessage resp = putAsync.Result;
+                if (resp.IsSuccessStatusCode)
+                {
+                    string jsonResStr = resp.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
     }
 }
