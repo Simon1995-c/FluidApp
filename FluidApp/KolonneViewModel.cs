30ee597ce9042bd9e424a303364af61e7b28cdef
Resul134 <resulkurrr134@gmail.com>
2019-05-06 09:21:42 +0200

    Revert "Merge branch 'master' of https://github.com/GretheSlushice/FluidApp"
    This reverts commit 5e5208a9b44f581a5922b61e36952b2288c1edcd, reversing
    changes made to 99c7b0a77055e52cafb0475ba27d616566388a6c.

--- FluidApp/KolonneViewModel.cs
+++ FluidApp/KolonneViewModel.cs
@@ -1,40 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using FluidApp.Annotations;
 using GalaSoft.MvvmLight.Command;
 using Models;
 
 namespace FluidApp
 {
-    class KolonneViewModel : INotifyPropertyChanged
+    class KolonneViewModel
     {
         public RelayCommand TilbageCommand { get; set; }
-        public RelayCommand SorterCommand { get; set; }
-        public ObservableCollection<Forside> _kolonneListe;
-
-        public ObservableCollection<Forside> KolonneListe
-        {
-            get { return _kolonneListe; }
-            set
-            {
-                _kolonneListe = value;
-                OnPropertyChanged();
-            }
-        }
+        public ObservableCollection<Forside> KolonneListe { get; set; }
 
         public KolonneViewModel()
         {
             TilbageCommand = new RelayCommand(Tilbage);
-            SorterCommand = new RelayCommand(SortDatasets);
             ipHandler h = new ipHandler();
 
             //If the IP isn't allowed -> send them to an error page
@@ -62,22 +46,11 @@ namespace FluidApp
 
             foreach (var att in tempForside.GetAll())
             {
+                
+                
                 data.Add(att);
             }
             return data;
-        }
-
-        public void SortDatasets()
-        {
-            KolonneListe = new ObservableCollection<Forside>(KolonneListe.OrderBy(e => e.Dato));
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
