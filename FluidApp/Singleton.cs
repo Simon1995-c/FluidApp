c2564a37f17dfebd4c0f4ff6636f42fb0c07627c
Resul134 <resulkurrr134@gmail.com>
2019-05-08 12:09:10 +0200

    Singleton

--- FluidApp/Singleton.cs
+++ FluidApp/Singleton.cs
@@ -1,29 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Security.Cryptography.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using FluidApp;
+using FluidApp.Annotations;
 using Models;
 
 namespace FluidApp
 {
-    public sealed class Singleton
+    public sealed class Singleton : INotifyPropertyChanged
     {
+        //logger ind, find rollen og gem rollen.
         private static Singleton _instance = new Singleton();
-        private ObservableCollection<Administrator> administrators;
+        private Administrator admin;
+        public LoginViewModel ROLLE { get; set; }
         public static Singleton Instance
         {
             get { return _instance; }
-
 
         }
 
         private Singleton()
         {
-            administrators = new ObservableCollection<Administrator>();
+            ROLLE = new LoginViewModel();
+        }
+
+
+        
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 
+        
 }
+
+
