1e9fcb1eb466662aa567d54d5e93f03d8f8ce6fa
Resul134 <resulkurrr134@gmail.com>
2019-05-08 10:48:27 +0200

    Singleton unit test and class

--- FluidApp/Singleton.cs
+++ FluidApp/Singleton.cs
@@ -1,46 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Security.Cryptography.Core;
+using Models;
 
 namespace FluidApp
 {
     public sealed class Singleton
     {
-        //The volatile keyword indicates that
-        //a field might be modified by multiple threads,
-        //that are executing at the same time
-        private static volatile Singleton instance;
-        private static object syncRoot = new Object();
+        private static Singleton _instance = new Singleton();
+        private ObservableCollection<Administrator> administrators;
+        public static Singleton Instance
+        {
+            get { return _instance; }
+
+
+        }
 
         private Singleton()
         {
-            //Write code (First)
-            //Remember: instance = new Singleton(); (At the end)
-        }
-
-
-
-        //The instantiation is not performed until an object asks for an instance;
-        //this approach is referred to as lazy instantiation.
-        //Lazy instantiation avoids instantiating unnecessary singletons when the application starts.
-        public static Singleton Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    lock (syncRoot)
-                    {
-                        if (instance == null)
-                            instance = new Singleton();
-                    }
-                }
-
-                return instance;
-            }
+            administrators = new ObservableCollection<Administrator>();
         }
     }
+
 }
